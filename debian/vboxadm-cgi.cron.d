a84542190de24d86eb67fa765808eea95753599f
Dominik Schulz <dominik.schulz@gauner.org>
2012-08-18 22:02:05 +0200

    Add dmarc cronjob.

--- debian/vboxadm-cgi.cron.d
+++ debian/vboxadm-cgi.cron.d
@@ -2,3 +2,4 @@
 # Regular cron jobs for the vboxadm-cgi package
 #
 0 4	* * *	root	[ -x /usr/lib/vboxadm/bin/cleanup ] && /usr/lib/vboxadm/bin/cleanup
+#0 4	* * *	root	[ -x /usr/lib/vboxadm/bin/dmarc ] && /usr/lib/vboxadm/bin/dmarc
